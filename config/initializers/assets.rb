8159aab1fb08245a9787ddf07e5477e0d5733029
sinharahul <sinharahul@gmail.com>
2016-01-16 12:35:09 -0500

    moved jquery to vendor/assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.min.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.structure.min.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.theme.min.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
