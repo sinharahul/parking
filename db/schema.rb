d708422b8a4781da313570c2b5ba17c5495c39d9
sinharahul <sinharahul@gmail.com>
2016-01-23 15:20:44 -0500

    making login mandatory

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20160120032611) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "deletes", force: :cascade do |t|
-    t.string   "guser"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "gusers", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
