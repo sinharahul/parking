f066b43a34e8ea54e98f00f77a6a37274b67b590
sinharahul <sinharahul@gmail.com>
2016-01-26 21:15:02 -0500

    fixing devise tests

--- test/models/car_registration_test.rb
+++ test/models/car_registration_test.rb
@@ -4,4 +4,7 @@ class CarRegistrationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "create new car registration" do
+    car=CarRegistration.new
+  end
 end
